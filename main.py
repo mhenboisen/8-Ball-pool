4cbf4c1df6da0a71eedcbdab8d57496cade63ab1
mhenboisen <q15mhenboisen@wgsf.net>
2025-09-23 12:58:15 +0100

    Update main.py
    Created a window pop up but need to read documentation more thoroughly in order to continue with the project

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+import pygame_functions
 
+#variables
+
+objects = []
+balls = []
+foul = False
+gameOver = False
+turn = 0
+GRAVITY = 9.81
+SPINNING_FRICTION = 0.2
+ROLLING_FRICTION = 0.1
+RADIUS = 10
+BOARD_WIDTH = 1500
+BOARD_HEIGHT = 750
+
+
+#subprograms
+
+#classes
+
+class Object:
+    def __init__(self,Pxpos,Pypos,Psprite):
+        self.xpos = Pxpos
+        self.ypos = Pypos
+        self.sprite = Psprite
+
+    def draw(self):
+        pass
+
+
+        
+        
+
+#MAIN**********************************
+
+pygame_functions.screenSize(1600,900)
+
+y = Object(0,0,'table.piskel')
+
+
+pygame_functions.endWait()
